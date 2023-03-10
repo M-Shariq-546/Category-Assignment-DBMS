04394fc02d75b10c80bb6fad40940effabd417b1
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2023-03-10 15:15:00 +0500

    Final Assignment

--- assignment.py
+++ assignment.py
@@ -5,39 +5,51 @@ import sqlite3 as s3
 win = Tk()
 win.title("Category and Subcategory by M. Shariq Shafiq")
 
-category = Label(win, text = "Enter Category name : ")
-category.pack()
 
-parent_id = Label(win, text = "Enter Parent id : ")
-parent_id.pack()
+category = str(input("Enter Category name : "))
+#category = Label(win, text = "Enter Category name : ")
+#category.pack()
+parent_id = str(input("Enter Parent id : "))
+#parent_id = Label(win, text = "Enter Parent id : ")
+#parent_id.pack()
 
 
-# create_table ='''create table categories (
-#                     id INTEGER PRIMARY KEY auto incremented,
-#                     c_name varchar,
-#                     parent_id INTEGER,
-#                     FOREIGN KEY (parent_id) REFERENCES categories(id) )'''
+create_table ='''CREATE TABLE IF NOT EXISTS category
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 category_name TEXT,
+                 parent_id INTEGER,
+                 FOREIGN KEY (parent_id) REFERENCES category(id))'''
 
-con = s3.connect('categories.db' , timeout=10)
+con = s3.connect('category.db' , timeout=10)
 cur = con.cursor()
 
 data = [(category , parent_id)]
 
-sql = '''insert into categories (category , parent_id) values(? , ?)'''
+sql = '''insert into category (category_name , parent_id) values(? , ?)'''
 
-#res = cur.execute(create_table) 
+res = cur.execute(create_table) 
 
-#res1 = cur.executemany(sql , data)
+res1 = cur.executemany(sql , data)
 
-res3 = '''select * from categories
-    where parent_id is 2'''
+res2 = '''select * from category
+    where parent_id is "null"'''
 
-res4 = cur.execute(res3)
+res3 = '''select * from category
+    where parent_id is 1'''
 
-#print(res4)
-res_final = cur.fetchall()
 
-print(res_final)
+res4 = cur.execute(res2)
+
+res_final_1 = cur.fetchall()
+
+res5 = cur.execute(res3)
+
+
+res_final_2 = cur.fetchall()
+print("Main Category : ")
+print(res_final_1)
+print("Sub-Categories :")
+print(res_final_2)
 
 con.commit()
 
